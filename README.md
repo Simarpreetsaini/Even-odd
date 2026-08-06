f852f13b3c1262e892f5e137c0c88236058e1d57
Simarpreet Singh <harkiratsaini47@gmail.com>
2026-08-07 02:46:51 +0530

    files upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# Even or Odd Checker
+
+## Objective
+
+This program checks whether a number is even or odd using if-else statements.
+
+## How it Works
+
+- I created four number variables.
+- I used if-else to check each number.
+- If the number is divisible by 2, it is even.
+- Otherwise, it is odd.
+- The result is displayed in the console and on the webpage.
+
+## Output
+
+The number 5 is odd.
+The number 10 is even.
+The number 7 is odd.
+The number 12 is even.
---
+++ script.js
@@ -0,0 +1,41 @@
+let num1 = 5;
+let num2 = 10;
+let num3 = 7;
+let num4 = 12;
+
+let message;
+
+if (num1 % 2 === 0) {
+  message = "The number " + num1 + " is even.";
+} else {
+  message = "The number " + num1 + " is odd.";
+}
+
+console.log(message);
+document.write(message + "<br>");
+if (num2 % 2 === 0) {
+  message = "The number " + num2 + " is even.";
+} else {
+  message = "The number " + num2 + " is odd.";
+}
+
+console.log(message);
+document.write(message + "<br>");
+
+if (num3 % 2 === 0) {
+  message = "The number " + num3 + " is even.";
+} else {
+  message = "The number " + num3 + " is odd.";
+}
+
+console.log(message);
+document.write(message + "<br>");
+
+if (num4 % 2 === 0) {
+  message = "The number " + num4 + " is even.";
+} else {
+  message = "The number " + num4 + " is odd.";
+}
+
+console.log(message);
+document.write(message + "<br>");
